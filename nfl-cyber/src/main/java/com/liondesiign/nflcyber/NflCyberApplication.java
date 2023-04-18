be9b5b5a49d2b65d38926d003f07b3f20e08396a
Lionel Morgan <lionel.morgan@revature.net>
2023-04-17 21:28:27 -0400

    nfl draft app

---
+++ nfl-cyber/src/main/java/com/liondesiign/nflcyber/NflCyberApplication.java
@@ -0,0 +1,13 @@
+package com.liondesiign.nflcyber;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NflCyberApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NflCyberApplication.class, args);
+	}
+
+}
---
+++ nfl-cyber/src/test/java/com/liondesiign/nflcyber/NflCyberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.liondesiign.nflcyber;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NflCyberApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
